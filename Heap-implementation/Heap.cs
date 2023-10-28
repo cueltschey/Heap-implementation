422b8ad8afd5ac1c2471bb3096f732e76cf3d88e
cueltschey <chaseuelt@gmail.com>
2023-10-27 20:13:13 -0500

    Added insert and remove

--- Heap-implementation/Heap.cs
+++ Heap-implementation/Heap.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,42 +16,42 @@ namespace Heap_implementation
     {
         private Node root;
         private int depth;
+        private string minOrMax;
         public Heap(int start)
         {
             root = new Node(start);
             depth = 0;
         }
-        private string display(Node node, int curdepth = 0)
+        // private facing display that prints vertically in preorder
+        private void display(Node node, int curdepth = 0)
         {
-            Console.Write("\n");
-            string right;
-            string left;
             if (curdepth < depth)
             {
-                left = insert(node.children[0], data, curdepth + 1);
-                if (left)
-                {
-                    return true;
-                }
-                right = insert(node.children[1], data, curdepth + 1);
-                if (right)
-                {
-                    return true;
-                }
-                return false;
+                Console.WriteLine(node.data);
+                display(node.children[0], curdepth + 1);
+                display(node.children[1], curdepth + 1);
             }
             else
             {
-                if (node.degree() < 2)
+                if (node.degree() == 0)
                 {
-                    node.children.Add(new Node(data));
-                    return true;
+                    Console.WriteLine(node.data);
                 }
                 else
                 {
-                    return false;
+                    Console.WriteLine(node.data);
+                    foreach(Node n in node.children)
+                    {
+                        Console.WriteLine(n.data);
+                    }
                 }
             }
+        }
+        //public facing display that prints in preorder
+        public void display()
+        {
+            Console.WriteLine("Depth: " + depth);
+            display(root);
         }
         /*
          This insert uses recursion to perform the following steps:
@@ -97,38 +102,78 @@ namespace Heap_implementation
                 depth += 1;
                 insert(root, data);
             }
+
+            makeMin(root);
         }
 
-        private bool makeMin(Node node, int data, int curdepth = 0)
+        private void makeMin(Node node, int curdepth = 0)
         {
-            bool right;
-            bool left;
             if (curdepth < depth)
             {
-                left = insert(node.children[0], data, curdepth + 1);
-                if (left)
+                makeMin(node.children[0], curdepth + 1);
+                makeMin(node.children[1], curdepth + 1);
+                foreach (Node n in node.children)
                 {
-                    return true;
+                    if (n.data < node.data)
+                    {
+                        int inter = n.data;
+                        n.data = node.data;
+                        node.data = inter;
+                    }
                 }
-                right = insert(node.children[1], data, curdepth + 1);
-                if (right)
-                {
-                    return true;
-                }
-                return false;
             }
             else
             {
-                if (node.degree() < 2)
+                if(node.degree() != 0)
                 {
-                    node.children.Add(new Node(data));
-                    return true;
-                }
-                else
-                {
-                    return false;
+                    foreach(Node n in node.children)
+                    {
+                        if(n.data < node.data)
+                        {
+                            int inter = n.data;
+                            n.data = node.data;
+                            node.data = inter;
+                        }
+                    }
                 }
             }
         }
+        private void balance(Node node)
+        {
+            if(root.degree() == 2)
+            {
+                if (root.children[0].data < root.children[1].data)
+                {
+                    root.children[0].children[1]
+                }
+            }
+        }
+        private int? remove(Node node, int curdepth = 0)
+        {
+            int? right;
+            int? left;
+            if(curdepth < depth)
+            {
+                right = remove(node.children[1], curdepth + 1);
+                left = remove(node.children[0], curdepth + 1);
+                if(right != null)
+                {
+                    return right;
+                }
+                else
+                {
+                    return left;
+                }
+            }
+            else
+            {
+                if (node.degree() != 0)
+                {
+                    int res = node.children[-1].data;
+                    node.children.RemoveAt(-1);
+                    return res;
+                }
+            }
+            return null;
     }
 }
--- Heap-implementation/Program.cs
+++ Heap-implementation/Program.cs
@@ -2,7 +2,8 @@
 
 using Heap_implementation;
 
-Heap heap = new Heap(10);
+Heap heap = new Heap(10, "max");
+
 heap.insert(11);
 heap.insert(22);
 heap.insert(5);
@@ -10,4 +11,13 @@ heap.insert(60);
 heap.insert(12);
 heap.insert(77);
 heap.insert(4);
+heap.insert(7);
+heap.insert(10);
+heap.insert(55);
+heap.insert(20);
+heap.insert(1);
+heap.insert(45);
+heap.insert(11);
+
+heap.display();
 
